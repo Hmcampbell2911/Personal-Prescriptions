78a1670c4017b7f3cccfcea235b2aa9f5191d84d
Hannah Campbell (Troyer) <84107418+Hmcampbell2911@users.noreply.github.com>
2021-11-13 14:27:20 -0500

    I don't know if this was committed?

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GitHub
+{
+  //While loop, Master loop//
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true){
+                Console.WriteLine("Add new patient");
+            }
+        }
+    }
+}
+
+//Objects to store items//
+    public class Patient
+    {
+        public string PatientName {get; set;}
+        public string PrescriptionName {get; set;}
+        public string SideEffects {get; set;}
+//how do i add a TimeSpan string?//
+    class Program {
+        static void Main(string[] args)
+        {
+            Patient std = new Patient ()
+        }
+    }
+
+    }
+//Write to file//
+
+  
